d288e3cd35ef62cd55e289966115d5ad8663cf2b
root <p.mii.97@gmail.com>
2020-01-25 23:00:05 +0000

    hackiest code in the world

---
+++ Q3/add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Badly formatted arguments");
+	}
+
+	float num1 = atof(argv[1]);
+	float num2 = atof(argv[2]);
+	float result = num1 + num2;
+
+	fprintf( stdout, "%f", result );
+
+	return 0;
+}
--- Q3/main.py
+++ Q3/main.py
@@ -1,19 +1,32 @@
 import sys
+import subprocess
 
-operators = ["+", "-", "/", "*"]
+operators = ["+", "/", "*", "-"]
 mathThings = ["(", ")", operators]
 
 def evalPos(num1, num2):
-    print("plus")
+    command = "/code/Q3/add " + str(num1) + " " + str(num2)
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    return output
 
 def evalNeg(num1, num2):
-    print("neg")
+    command = "/code/Q3/sub " + str(num1) + " " + str(num2)
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    return output
 
 def evalDiv(num1, num2):
-    print("div")
+    command = "/code/Q3/div " + str(num1) + " " + str(num2)
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    return output
 
 def evalMul(num1, num2):
-    print("mul")
+    command = "/code/Q3/mul " + str(num1) + " " + str(num2)
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    return output
 
 def applyOperator(operator, num1, num2):
     switcher = {
@@ -31,12 +44,35 @@ def dumpReplace(equation):
         equation = equation.replace(mathThing, " " + mathThing + " ")
     return equation
 
+def remove_at(i, s):
+    return s[:i] + s[i+1:]
+
 def evaluate( expression ):
-    print(expression)
+    num1Neg = False
+    num2Neg = False
+
+    if expression[0] == "-":
+        expression = remove_at(0, expression)
+        num1Neg = True
+    firstOperator = -1
+    for operator in operators:
+        firstOperator = expression.find(operator)
+        if firstOperator >= 0:
+            break
+    if firstOperator >= 0:
+        if expression[firstOperator+1] == "-":
+            expression = remove_at(firstOperator+1, expression)
+            num2Neg = True    
+
     for operator in operators:
         if operator in expression:
-            applyOperator(operator, 1, 1)  
-            return str(123.0)
+            expression = expression.split(operator)
+            if num1Neg:
+                expression[0] = float(expression[0]) * -1.0
+            if num2Neg:
+                expression[1] = float(expression[1]) * -1.0
+            output = applyOperator(operator, float(expression[0]), float(expression[1]))  
+            return output
     return expression
 
 ### MAIN ###
@@ -64,7 +100,5 @@ for item in equation:
             stack[-1] = stack[-1] + item
         else:
             print("Input equation should start with a ( character")
-
-
 
 sys.stdout.write( result + "\n" )
